1d78969d047dac51562fa9b09489f8b7efe095c9
Alexandr Kalinkin <akalinkin84@yandex.ru>
2025-04-24 14:00:02 +0300

    6

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class PublishedManager(models.Manager): # определяем класс своего менеджера модели
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
+
+
+class Women(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager() # указываем менеджер objects, иначе он перестанет работать после определения своего менеджера
+    published = PublishedManager() # определяем свой менеджер
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-time_create'] # задать сортировку по-умолчанию(- означает в обратном порядке)
+        indexes = [
+            models.Index(fields=['-time_create']) # проиндексировать поле time_create с учетом сортировки(-)
+        ]
+
+    def get_absolute_url(self): # возвращает url каждой записи таблицы
+        return reverse('post', kwargs={'post_slug': self.slug})
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify # можем использовать шаблонные фильтры как функции
 
+from .models import Women
+
 
 # menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
 menu = [
     {'title': "О сайте", 'url_name': 'about'},
     {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -32,10 +35,13 @@ cats_db = [
 
 
 def index(request):
+    # posts = Women.objects.filter(is_published=1)
+    posts = Women.published.all()
+
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': posts,
         'cat_selected': 0
     }
     return render(request, 'women/index.html', context=data)
@@ -80,8 +86,16 @@ def about(request):
 #     return HttpResponse(f'<h1>Архив п годам</h1><p>{year}</p>')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отображение статьи с id: {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug) # получить 1-у запись из таблицы women по критерию pk=post_idб если запись не найдена генерирует 404
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1
+    }
+
+    return render(request, 'women/post.html', data)
 
 
 def addpage(request):
