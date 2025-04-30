fcfb37ad3058e83160163c6f78ad132b5323f9a2
Alexandr Kalinkin <akalinkin84@yandex.ru>
2025-04-30 12:05:53 +0300

    11

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 import women.views as views
 from women.models import Category, TagPost
@@ -14,10 +15,10 @@ register = template.Library()
 @register.inclusion_tag('women/list_categories.html') # регистрируем свой тег как включающий тег(возвращает фрагмент html страницы) и указываем путь к шаблону который возвратится
 def show_categories(cat_selected=0):
     # cats = views.cats_db
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 
 @register.inclusion_tag('women/list_tags.html') # регистрируем свой тег как включающий тег(возвращает фрагмент html страницы) и указываем путь к шаблону который возвратится
 def show_all_tags():
-    return {'tags': TagPost.objects.all()}
+    return {'tags': TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
