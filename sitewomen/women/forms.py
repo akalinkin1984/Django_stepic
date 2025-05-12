84289c6415f586855111850e25b56b979865deb8
Alexandr Kalinkin <akalinkin84@yandex.ru>
2025-05-12 13:52:46 +0300

    16

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from .models import Category, Husband
 
 
-class AddPostForm(forms.Form): # определяем класс для отображения формы
+class AddPostForm(forms.Form): # определяем класс для отображения формы не связанной с моделью
     title = forms.CharField(max_length=255)
     slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False) # многострочный текст, required - необязательно заполнять
+    content = forms.CharField(widget=forms.Textarea(), required=False) # многострочный текст, required - необязательное поле
     is_published = forms.BooleanField(required=False)
     cat = forms.ModelChoiceField(queryset=Category.objects.all()) # выпадающий список
     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False) # выпадающий список
+    
